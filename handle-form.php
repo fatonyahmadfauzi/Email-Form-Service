65a028684d350db499d405eb1d51f09e6b0bf08b
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-03-06 17:50:08 +0700

    test di railway

--- handle-form.php
+++ handle-form.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // CORS Configuration
-header("Access-Control-Allow-Origin: https://fatonyahmadfauzi.netlify.app");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
