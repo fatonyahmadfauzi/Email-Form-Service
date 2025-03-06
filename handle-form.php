36bf6206c7adf8d3ebe5f1eee883cfd3d19d7716
fatonyahmadfauzi <fatonyahmadfauzi@gmail.com>
2025-03-06 17:37:08 +0700

    test di railway

--- handle-form.php
+++ handle-form.php
@@ -8,7 +8,6 @@ header("Access-Control-Allow-Origin: https://fatonyahmadfauzi.netlify.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Tangani preflight request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
